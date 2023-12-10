7604681542122f2bb297b3ab528eba023247b08d
EC2 Default User <ec2-user@ip-172-31-3-243.ap-northeast-1.compute.internal>
2023-12-10 12:42:21 +0000

    DBに反映

---
+++ db/migrate/20231210122606_create_orders.rb
@@ -0,0 +1,14 @@
+class CreateOrders < ActiveRecord::Migration[6.1]
+  def change
+    create_table :orders do |t|
+      t.integer :customer_id, null: false
+      t.string :ship_postalcode, null: false
+      t.string :ship_address, null: false
+      t.string :ship_name, null: false
+      t.integer :postage, null: false
+      t.integer :payment_amount, null: false
+      t.integer :payment_method, null: false
+      t.timestamps
+    end
+  end
+end
