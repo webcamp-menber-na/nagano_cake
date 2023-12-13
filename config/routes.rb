71e7cda934a9124d3531887c50e0ad3e75aa8219
EC2 Default User <ec2-user@ip-172-31-3-243.ap-northeast-1.compute.internal>
2023-12-13 15:08:53 +0000

    admin ルーディング追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,20 @@
 Rails.application.routes.draw do
   root 'public/homes#top'
-  #管理者用
-  devise_for :admin, skip: [:registrations, :passwords], controllers: {
-    sessions: "admin/sessions"
-  }
+    #管理者用
+    devise_for :admin, skip: [:registrations, :passwords], controllers: {
+      sessions: "admin/sessions"
+    }
   
-  #顧客用
-  devise_for :customers, skip: [:passwords], controllers: {
-    registrations: "public/registrations",
-    sessions: 'public/sessions'
-  }
+    #顧客用
+    devise_for :customers, skip: [:passwords], controllers: {
+      registrations: "public/registrations",
+      sessions: 'public/sessions'
+    }
   
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  namespace :admin do
+    get '' => 'homes#top'
+    resources :items,only: [:new, :index, :show, :edit]
+    resources :customers,only: [:index, :show, :edit]
+    resources :orders,only: [:show]
+  end
 end
