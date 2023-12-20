c532afe3ac7e0e6e020f91ad8698a83b47a14291
EC2 Default User <ec2-user@ip-172-31-3-243.ap-northeast-1.compute.internal>
2023-12-20 13:49:21 +0000

    登録情報編集画面終

--- app/controllers/public/registrations_controller.rb
+++ app/controllers/public/registrations_controller.rb
@@ -2,7 +2,7 @@
 
 class Public::RegistrationsController < Devise::RegistrationsController
   before_action :configure_sign_up_params, only: [:create]
-  # before_action :configure_account_update_params, only: [:update]
+  before_action :configure_account_update_params, only: [:update]
 
   # GET /resource/sign_up
   # def new
@@ -46,9 +46,9 @@ class Public::RegistrationsController < Devise::RegistrationsController
   end
 
   # If you have extra params to permit, append them to the sanitizer.
-  # def configure_account_update_params
-  #   devise_parameter_sanitizer.permit(:account_update, keys: [:attribute])
-  # end
+  def configure_account_update_params
+   devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name,:first_name_kana, :last_name_kana, :postal_code, :address, :telephone_number, ])
+  end
 
   # The path used after sign up.
   # def after_sign_up_path_for(resource)
@@ -59,5 +59,17 @@ class Public::RegistrationsController < Devise::RegistrationsController
   # def after_inactive_sign_up_path_for(resource)
   #   super(resource)
   # end
+  def update_resource(resource, params)
+    resource.update_without_password(params)
+  end
+
+  # 編集後のリダイレクト先を指定するメソッド
+  def after_update_path_for(resource)
+    public_customer_path(resource)
+  end
+  
+  def after_sign_up_path_for(resource)
+    public_customer_path(resource)
+  end
   
 end
