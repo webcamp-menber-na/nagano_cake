68ef1e0fec27a454dc31e823f38d9df55ba956bb
EC2 Default User <ec2-user@ip-172-31-3-243.ap-northeast-1.compute.internal>
2023-12-21 12:32:57 +0000

    退会画面終

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -14,7 +14,7 @@ class Public::CustomersController < ApplicationController
   
   def withdraw
     @customer = current_customer
-    @customer.withdrawal_status = true
+    @customer.is_active = false
     if @customer.save
       reset_session
       redirect_to root_path
