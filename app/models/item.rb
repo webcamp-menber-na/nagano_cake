9e8610caa7ac6ddfa232eb065677fc58eda989d0
EC2 Default User <ec2-user@ip-172-31-3-243.ap-northeast-1.compute.internal>
2023-12-28 12:38:54 +0000

    商品詳細終

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,8 +1,13 @@
 class Item < ApplicationRecord
+  has_many :cart_items, dependent: :destroy
   has_one_attached :image
   belongs_to :admin
+  
+  validates :item_image, presence: true
+
   
   def with_tax_price
     (price * 1.1).floor
   end
+  
 end
