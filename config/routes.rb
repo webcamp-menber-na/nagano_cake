ec08061e7a07b0b940aa9a75d151abb3381c0beb
EC2 Default User <ec2-user@ip-172-31-3-243.ap-northeast-1.compute.internal>
2023-12-29 14:51:08 +0000

    会員情報編集画面終

--- config/routes.rb
+++ config/routes.rb
@@ -29,7 +29,7 @@ Rails.application.routes.draw do
   namespace :admin do
     get '' => 'homes#top'
     resources :items,only: [:new, :create, :index, :show, :edit, :update]
-    resources :customers,only: [:index, :show, :edit]
+    resources :customers,only: [:index, :show, :edit, :update]
     resources :orders,only: [:show]
   end
 end
